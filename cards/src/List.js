66c66b83320cf22d8d9e3298140ddd1488d86dd9
Ananya015 <jainananya78@gmail.com>
2021-04-19 14:56:59 +0530

    done

--- cards/src/List.js
+++ cards/src/List.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const List=(props)=>{
     return (
-        <div className="cardstyle bg-yellow ma4 dib pa4 tc ba bw1" >
+        <div className="cardstyle bg-yellow ma4 mr4 ml4 mv4 dib pa4 tc grow ba bw1" >
         <img src={props.image}/>
         <h1>{props.name}</h1>
         <p>{props.work}</p>
